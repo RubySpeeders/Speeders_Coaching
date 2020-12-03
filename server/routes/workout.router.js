466839a7df2b0bc31892f978696f1967daa3fc24
Sarah Peters <sarahnpeters@gmail.com>
2020-12-02 20:28:55 -0600

    adding promise.all for posting a workout on serverside

--- server/routes/workout.router.js
+++ server/routes/workout.router.js
@@ -37,37 +37,41 @@ router.get('/athlete/:id', (req, res) => {
 });
 
 // add a workout
-// router.post('/:id', rejectUnauthenticated, (req, res) => {
-//   const { date, description } = req.body;
-//   const athlete_id = req.params.id;
-//   const coach_id = req.user.id;
-//   let queryText = `INSERT INTO "workouts" (date, athlete_id, coach_id, description) VALUES ($1, $2, $3, $4) RETURNING id;`;
-//   let queryArray = [date, athlete_id, coach_id, description];
-//   pool
-//     .query(queryText, queryArray)
-//     .then((dbResponse) => {
-//       const workout_id = dbResponse.rows[0].id;
-//       for (let i = 0; i < race_type_list.length; i++) {
-//         const race_key = race_type_list[i];
-//         const race_value = req.body.race_type[race_key];
-//         if (race_value) {
-//           const queryText = `INSERT INTO "workout_details" (workout_id, repetitions, distance, pace, step) VALUES ($1, $2, $3, $4, $5);`;
-//           const queryArray = [athlete_info_id, race_key];
-//           athlete_race_array.push(pool.query(queryText, queryArray));
-//         }
-//       }
-//       Promise.all(athlete_race_array).then((dbResponse) => {
-//         res.sendStatus(201);
-//       });
-//      const repetitions = req.body.
-//       let queryText = ``;
-//       let queryArray = [workout_id];
-//       res.sendStatus(201);
-//     })
-//     .catch((error) => {
-//       console.log('error posting workout', error);
-//       res.sendStatus(500);
-//     });
-// });
+router.post('/add/:id', rejectUnauthenticated, (req, res) => {
+  try {
+    const { date, description } = req.body;
+    const athlete_id = req.params.id;
+    const coach_id = req.user.id;
+    let queryText = `INSERT INTO "workouts" (date, athlete_id, coach_id, description) VALUES ($1, $2, $3, $4) RETURNING id;`;
+    let queryArray = [date, athlete_id, coach_id, description];
+    pool.query(queryText, queryArray).then((dbResponse) => {
+      const workout_id = dbResponse.rows[0].id;
+      const workout_array = req.body.workout;
+      const new_workout_array = [];
+      //iterate through array of workout steps
+      for (let i = 0; i < workout_array.length; i++) {
+        const workout_step = workout_array[i].step;
+        const workout_rep = workout_array[i].rep;
+        const workout_distance = workout_array[i].distance;
+        const workout_pace = workout_array[i].pace;
+        const queryText = `INSERT INTO "workout_details" (workout_id, repetitions, distance, pace, step) VALUES ($1, $2, $3, $4, $5);`;
+        const queryArray = [
+          workout_id,
+          workout_rep,
+          workout_distance,
+          workout_pace,
+          workout_step,
+        ];
+        new_workout_array.push(pool.query(queryText, queryArray));
+      }
+      Promise.all(new_workout_array).then(() => {
+        res.sendStatus(201);
+      });
+    });
+  } catch (error) {
+    console.log('error posting workout', error);
+    res.sendStatus(500);
+  }
+});
 
 module.exports = router;
