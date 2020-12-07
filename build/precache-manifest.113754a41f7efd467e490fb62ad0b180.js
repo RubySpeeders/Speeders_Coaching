eb1e6453493378a27f9beac1a1bfb169111c5058
Sarah Peters <sarahnpeters@gmail.com>
2020-12-07 16:25:57 -0600

    production build for client

---
+++ build/precache-manifest.113754a41f7efd467e490fb62ad0b180.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92eb08b5a9e2376946f2c3d06b83bc26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e05a8684dd6e5160052",
+    "url": "/static/css/main.8efd3302.chunk.css"
+  },
+  {
+    "revision": "37d308e52516fc7979ac",
+    "url": "/static/js/2.93ea8802.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.93ea8802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e05a8684dd6e5160052",
+    "url": "/static/js/main.a86ed2e3.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  },
+  {
+    "revision": "755770155244ff1f9900670f13e73e96",
+    "url": "/static/media/run_background.75577015.jpg"
+  }
+]);
