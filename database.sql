96e7a7364f3e5db81f204e906e9d3f7d2e05ff42
Sarah Peters <sarahnpeters@gmail.com>
2020-12-09 16:17:34 -0600

    updating order of queries in database.sql

--- database.sql
+++ database.sql
@@ -4,26 +4,6 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE,
-    "password" VARCHAR (1000),
-    "first_name" VARCHAR (80)NOT NULL,
-    "last_name" VARCHAR (80) NOT NULL,
-    "city" VARCHAR (80),
-    "email" VARCHAR (80) NOT NULL,
-    "DOB" DATE,
-    "gender" VARCHAR (40),
-    "role_id" INT REFERENCES "roles",
-    "strava_id" VARCHAR (80)
-);
-
-CREATE TABLE "roles" (
-	"id" SERIAL PRIMARY KEY,
-	"role" VARCHAR,
-	"access_level" int
-	);
-
 INSERT INTO "roles" (role, access_level) VALUES ('coach', 1), ('athlete', 0);
 
 CREATE TABLE "days_of_week" (
@@ -67,6 +47,26 @@ CREATE TABLE "workout_steps" (
 );
 
 INSERT INTO "workout_steps" (step) VALUES ('Warm up'), ('Interval'), ('Recovery'), ('Cool down');
+
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE,
+    "password" VARCHAR (1000),
+    "first_name" VARCHAR (80)NOT NULL,
+    "last_name" VARCHAR (80) NOT NULL,
+    "city" VARCHAR (80),
+    "email" VARCHAR (80) NOT NULL,
+    "DOB" DATE,
+    "gender" VARCHAR (40),
+    "role_id" INT REFERENCES "roles",
+    "strava_id" VARCHAR (80)
+);
+
+CREATE TABLE "roles" (
+	"id" SERIAL PRIMARY KEY,
+	"role" VARCHAR,
+	"access_level" int
+	);
 
 CREATE TABLE "athlete_info" (
     "id" SERIAL PRIMARY KEY,
